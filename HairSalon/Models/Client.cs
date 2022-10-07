81bd923f5fae7eefb820a4c0f45fe4578dff682f
Samuel Majerus <ladolego@gmail.com>
2022-10-07 10:48:09 -0700

    Adds 'About' field to model.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,7 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
+    public string About { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
