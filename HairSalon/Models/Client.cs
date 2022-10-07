9185b4ab94b9ccda25b35f1ca186afff736c67f5
Samuel Majerus <ladolego@gmail.com>
2022-10-07 09:54:36 -0700

    Renames the Main Project file and the associated Tests file, removes the template's 'sql' file (already imported it to MySQL). Renames the Controllers and Models for necessary consistency.    Renames most of the namespaces and variables in the Main Project folder's files. [WIP:  Finish renaming namespaces and variables for each 'View' file.  Finish Database, export DB from Workbench to Main Project folder.]

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,13 @@
+namespace HairSalon.Models
+{
+  public class Client 
+  {
+    public int ClientId { get; set; }
+    // public string Name { get; set; }
+    // public int Rating { get; set; }
+    // public int Cost { get; set; }
+    // public string Review { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
