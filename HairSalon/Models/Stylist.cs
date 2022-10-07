ceeaabe990f263d76aa0b13dac541cb614fbde1d
Samuel Majerus <ladolego@gmail.com>
2022-10-07 10:45:16 -0700

    Adds field 'StylistDetails' to model.  Includes it in View files 'Create', 'Edit', and 'Show' for ''Stylists''.

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -11,6 +11,7 @@ namespace HairSalon.Models
 
     public int StylistId { get; set; }
     public string StylistName { get; set; }
+    public string StylistDetails { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
   }
 }
